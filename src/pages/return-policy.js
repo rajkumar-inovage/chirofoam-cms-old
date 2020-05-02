975590b77f43d962736bb779c584cb69223a7049
Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>
2020-05-02 20:04:30 +0530

    Signed-off-by: Raj-Inovexia-Shopify-Chirofoam <50763437+rajkumar-inovage@users.noreply.github.com>

--- src/pages/return-policy.js
+++ src/pages/return-policy.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {useStaticQuery, Link } from 'gatsby'
+import ReactHtmlParser from 'react-html-parser'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import { Container, Jumbotron, Row, Col } from 'reactstrap'
@@ -7,18 +8,38 @@ import ScrollAnimation from 'react-animate-on-scroll'
 import SEO from '~/components/seo'
 
 export default props => {
+  const { wordpressPage } = useStaticQuery(
+    graphql`
+      query {
+        wordpressPage(acf: {}, wordpress_id: {eq: 384}) {
+            acf {
+                banner_title
+                banner_subtitle
+                block1_title
+                block1_description
+                block2_title
+                block2_description
+                block3_title
+                block3_description
+                block4_title
+                block4_description
+              }
+          }
+      }
+    `
+    )
   return (
     <>
       <SEO
-        title="CHIROFOAM™ MEMORY FOAM MATTRESS"
-        description="100 Night In Home Sleep Trial – Full Money Back Returns"
+        title={wordpressPage.acf.banner_title}
+        description={wordpressPage.acf.banner_subtitle}
       />
       <Header />
       <section className="blue-bg charity">
         <Jumbotron className="mb-0 text-center text-white bg-transparent space-1">
           <ScrollAnimation animateIn="fadeInUp">
             <h1 className="font-weight-bold display-5 erbaum-bold text-uppercase pt-5 space-2">
-              CHIROFOAM™ MEMORY FOAM MATTRESS
+              {wordpressPage.acf.banner_title}
             </h1>
           </ScrollAnimation>
           <ScrollAnimation animateIn="fadeInUp">
@@ -26,7 +47,7 @@ export default props => {
               className="space-4 proxima-r text-white m-auto text-center px-0 px-sm-5 px-lg-5 px-xl-5 pt-2 pt-sm-4"
               style={{ width: '90%', fontSize: '26px' }}
             >
-              100 Night In Home Sleep Trial – Full Money Back Returns
+              {wordpressPage.acf.banner_subtitle}
             </p>
           </ScrollAnimation>
         </Jumbotron>
@@ -37,116 +58,35 @@ export default props => {
             <div className="col-12">
               <ScrollAnimation animateIn="fadeInUp">
                 <h3 className="lead-text lead-sm-text lead-md-text lead-lg-text lead-xl-text erbaum-bold font-weight-bold color-primary space-2 text-center">
-                  IN HOME SLEEP TRIAL
+                  {wordpressPage.acf.block1_title}
                 </h3>
               </ScrollAnimation>
               <p className="filson-pro-reg color-secondary pt-5 text-1">
-                Try the Chirofoam™ mattress in the comfort of your home for 100
-                nights and see what a difference it could make in the quality of
-                your sleep. From back pain relief, increased muscle recovery,
-                and overall sleep comfort, we are sure Chirofoam™ is the best
-                mattress you will ever buy. If for whatever reason you do not
-                feel like the Chirofoam™ mattress is right for you, you may
-                return it and we will give you 100% of your money back.
+                {ReactHtmlParser(wordpressPage.acf.block1_description)}
               </p>
               <ScrollAnimation animateIn="fadeInUp">
                 <h3 className="lead-text lead-sm-text lead-md-text lead-lg-text lead-xl-text erbaum-bold font-weight-bold color-primary space-2 text-center pt-5">
-                  HOW DOES THE SLEEP TRIAL WORK?
+                  {wordpressPage.acf.block2_title}
                 </h3>
               </ScrollAnimation>
               <p className="filson-pro-reg color-secondary pt-5 text-1">
-                You have up to 100 nights to try the Chirofoam™ Memory Foam
-                Mattress in your home to decide if it is the best mattress you
-                have ever slept on. The sleep trial starts from the day you
-                receive your mattress and ends on the 101st day. If for some
-                reason you are not completely satisfied with the mattress, you
-                may return the mattress to receive 100% of your money back.
-              </p>
-              <p className="filson-pro-reg color-secondary pt-5 text-1">
-                Please note that you cannot return your mattress within the
-                first 21 nights, in order to let your body get adjusted to the
-                mattress. The National Sleep Association suggests that people
-                usually take about 21 days to get adjusted to their new
-                mattress. Since you have been sleeping on your old mattress for
-                many years, your body will take some time to get adjusted and
-                start to feel better on the Chirofoam™ mattress.
+                {ReactHtmlParser(wordpressPage.acf.block2_description)}
               </p>
               <ScrollAnimation animateIn="fadeInUp">
                 <h3 className=" lead-text lead-sm-text lead-md-text lead-lg-text lead-xl-text erbaum-bold font-weight-bold color-primary space-2 text-center pt-5">
-                  RETURN PROCEDURES
+                  {wordpressPage.acf.block3_title}
                 </h3>
               </ScrollAnimation>
-              <p className="filson-pro-reg color-secondary pt-5 text-1">
-                In order to return your mattress, you must contact us by email
-                to request a return authorization. We will provide you with an
-                RA#
-              </p>
-              <p className="filson-pro-reg color-secondary text-1">
-                We will work with you to make this process as quick and simple
-                as possible.
-              </p>
-              <p className="filson-pro-reg color-secondary text-1">
-                <ul className="pl-0 pl-sm-2 pl-md-2 pl-lg-3 pl-xl-3">
-                  <li>
-                    For returns in the Greater Toronto Area, we can schedule a
-                    pick-up with our partner courier company.
-                  </li>
-                  <li>
-                    For returns outside the Greater Toronto Area, customers are
-                    responsible for selecting a courier and sending the mattress
-                    back to our headquarters in Toronto.
-                  </li>
-                  <li>
-                    Customers also have the option to donate their mattress to a
-                    registered charity and/or non-profit organization and
-                    provide us with a donation receipt in order to receive their
-                    refund. (As part of our environmentally friendly initiative,
-                    we ensure that our returned mattresses are always donated,
-                    and never end up in the landfill.)
-                  </li>
-                </ul>
-              </p>
-              <p className="filson-pro-reg color-secondary text-1">
-                Please ensure that your mattress is clean and free from any
-                stains. Refunds are issued once the mattress is received and
-                inspected for damages and stains or upon receiving the
-                charitable receipt. Mattresses must be returned in good
-                condition, with all instructions, warranties, and original
-                serial numbers (located on the mattress new material tags as
-                well as the label attached to the box your mattress was shipped
-                in). Note that returned mattress cannot be resold as they are
-                considered personal care items. We will strive to donate your
-                mattress to a charity or foundation which accepts used
-                mattresses.
-              </p>
-              <p className="filson-pro-reg color-secondary text-1">
-                Please state the reason for your return when contacting us as we
-                use this information to improve our products and your feedback
-                is greatly appreciated.
-              </p>
-              <p className="filson-pro-reg color-secondary text-1">
-                Carefully package the mattress using the original bag, or any
-                other heavy duty mattress bag, and/or mattress box for extra
-                protection.
-              </p>
-              <p className="filson-pro-reg color-secondary text-1">
-                Once we receive your mattress and confirm the return, we will
-                process your refund in full on the original method of payment
-                used for purchase within 2-5 business days. Please note that it
-                may take a little longer for the funds to process and display
-                back on to your original method of payment.
-              </p>
+              <div className="">
+                  {ReactHtmlParser(wordpressPage.acf.block3_description)}
+              </div>
               <h3 className="lead-text lead-sm-text lead-md-text lead-lg-text lead-xl-text erbaum-bold font-weight-bold color-primary space-2 text-center pt-5">
                 <ScrollAnimation animateIn="fadeInUp">
-                  AFTER THE 100 NIGHT SLEEP TRIAL
+                  {wordpressPage.acf.block4_title}
                 </ScrollAnimation>
               </h3>
               <p className="filson-pro-reg color-secondary pt-5 text-1">
-                The Chirofoam™ Memory Foam Mattress is protected by our 15 year
-                total warranty protection guarantee. This covers the mattress
-                from any defects in workmanship and quality, for a period of 15
-                years from the purchase date. Please see our warranty details
-                for more information.
+                {ReactHtmlParser(wordpressPage.acf.block4_description)}
               </p>
             </div>
           </Row>
